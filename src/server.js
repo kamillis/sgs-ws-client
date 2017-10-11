f407c2e55fd4c2136830b280c53a4bb616ce4e07
Kamil Lis <kamil.lis@schibsted.pl>
2017-10-11 17:46:59 +0200

    Add handshake data

--- src/server.js
+++ src/server.js
@@ -1,27 +1,48 @@
 import Koa from 'koa';
+import qs from 'qs';
 import io from 'socket.io-client';
 import getServerInfo from './handlers/getServerInfo';
 import getPlayersStats from './handlers/getPlayersStats';
+import queryGameServer from './utils/queryGameServer';
 import config from './config';
 
 const server = new Koa();
 
-const socket = io(`${config.SERVER_URL}?type=game`, {
-  transports: ['websocket'],
-  path: '/ws',
-});
+(async () => {
+  const {
+    name,
+    map,
+    game,
+    query: { host, port },
+  } = await queryGameServer();
 
-socket.on('connect_error', err => console.log(err));
-socket.on('connect', () => console.log('Socket is connected'));
-socket.on('disconnect', () => console.log('Socket is disconnected'));
+  const handshakeData = qs.stringify({
+    serverId: config.serverId,
+    type: 'game',
+    name,
+    map,
+    game,
+    host,
+    port,
+  });
 
-socket.on('get_server_info', async () => getServerInfo(socket));
-socket.on('get_players_stats', async () => getPlayersStats(socket));
+  const socket = io(`${config.SERVER_URL}?${handshakeData}`, {
+    transports: ['websocket'],
+    path: '/ws',
+  });
 
-setInterval(async () => {
-  await getServerInfo(socket);
-  await getPlayersStats(socket);
-}, 5000);
+  socket.on('connect_error', err => console.log(err));
+  socket.on('connect', () => console.log('Socket is connected'));
+  socket.on('disconnect', () => console.log('Socket is disconnected'));
+
+  socket.on('get_server_info', async () => getServerInfo(socket));
+  socket.on('get_players_stats', async () => getPlayersStats(socket));
+
+  setInterval(async () => {
+    await getServerInfo(socket);
+    await getPlayersStats(socket);
+  }, 5000);
+})();
 
 export default server;
 
