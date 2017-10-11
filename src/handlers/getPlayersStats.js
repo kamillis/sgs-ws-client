b1509146dc9262ac6365c0d1995687ab49793446
Kamil Lis <kamil.lis@schibsted.pl>
2017-10-11 15:58:21 +0200

    Add serverId

--- src/handlers/getPlayersStats.js
+++ src/handlers/getPlayersStats.js
@@ -1,10 +1,11 @@
 import queryPlayersStats from '../utils/queryPlayersStats';
 
 export default async function getPlayersStats(socket) {
+  const serverId = socket.id;
   try {
     const stats = await queryPlayersStats();
-    socket.emit('players_stats', stats);
+    socket.emit('players_stats', { serverId, data: stats });
   } catch (err) {
-    socket.emit('players_stats_error', err);
+    socket.emit('players_stats_error', { serverId, err });
   }
 }
--- src/handlers/getServerInfo.js
+++ src/handlers/getServerInfo.js
@@ -1,10 +1,11 @@
 import queryGameServer from '../utils/queryGameServer';
 
 export default async function getServerInfo(socket) {
+  const serverId = socket.id;
   try {
     const state = await queryGameServer();
-    socket.emit('server_info', state);
+    socket.emit('server_info', { serverId, data: state });
   } catch (err) {
-    socket.emit('server_info_error', err);
+    socket.emit('server_info_error', { serverId, err });
   }
 }
